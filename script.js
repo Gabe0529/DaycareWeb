18de8872bf3f378b735683e1f394b23c078ebc79
Gabe0529 <146754016+Gabe0529@users.noreply.github.com>
2024-01-21 00:11:13 -0500

    navbar update

--- script.js
+++ script.js
@@ -2,14 +2,21 @@
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
+    document.getElementById("myBtn").classList.toggle("none");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
     if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
         myDropdown.classList.remove('show');
       }
     }
-  }
+    if (!e.target.matches('.dropbtn')) {
+        var myBtn = document.getElementById("myBtn");
+        if (myBtn.classList.contains('none')) {
+        myBtn.classList.remove('none');
+      }
+    }
+}
